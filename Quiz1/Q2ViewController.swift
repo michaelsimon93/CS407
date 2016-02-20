b19f0e46de95573fe2f6bf3319d0e431723dd7b0
michaelsimon93 <michaelsimon93@gmail.com>
2016-02-19 22:54:38 -0600

    Blank Quiz

---
+++ Quiz1/Q2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Q2ViewController.swift
+//  Quiz1
+//
+//  Created by Michael Simon on 2/19/16.
+//  Copyright (c) 2016 Michael Simon. All rights reserved.
+//
+
+import UIKit
+
+class Q2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
